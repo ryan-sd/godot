65b319d3dd3049cef49c853a98ec730679a72850
Ryan_SD <theryfilmsgamermail@gmail.com>
2020-09-16 21:30:06 -0700

    hotkey functioning

--- modules/hotkey/hotkey.cpp
+++ modules/hotkey/hotkey.cpp
@@ -1,34 +1,50 @@
 #include "hotkey.h"
 
-void Hotkey::register_hotkey(int hotkey_id, int modifier, int virtual_key) {
+void Hotkey::register_hotkey(int hotkey_id, int modifier, int virtual_key, String input_name) {
 	RegisterHotKey(hWnd, hotkey_id, modifier, virtual_key);
+	input_map_entry entry;
+	entry.input_name = input_name;
+	entry.hotkey_id = hotkey_id;
+	input_map.push_back(entry);
 }
 
 void Hotkey::unregister_hotkey(int hotkey_id) {
 	UnregisterHotKey(hWnd, hotkey_id);
 }
 
+void Hotkey::test_hotkey() {
+	print_line("hotkey testing initiated");
+	hWnd = GetActiveWindow();
+	MSG msg = { 0 };
+
+	int length = input_map.size();
+
+	while (GetMessage(&msg, hWnd, 0, 0) != 0) {
+		if (msg.message == WM_HOTKEY) {
+			for (int x = 0; x < length; x++) {
+				input_map_entry entry = input_map.get(x);
+				int id = entry.hotkey_id;
+				if (id == msg.wParam) {
+					Input::get_singleton()->action_press(entry.input_name, 1.0f);
+					Input::get_singleton()->action_release(entry.input_name);
+				}
+			}
+		}
+	}
+}
+
 void Hotkey::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("register_hotkey", "hotkey_id", "modifier", "virtual_key"), &Hotkey::register_hotkey);
+	ClassDB::bind_method(D_METHOD("register_hotkey", "hotkey_id", "modifier", "virtual_key", "input_name"), &Hotkey::register_hotkey);
 	ClassDB::bind_method(D_METHOD("unregister_hotkey", "hotkey_id"), &Hotkey::unregister_hotkey);
 	ClassDB::bind_method(D_METHOD("test_hotkey"), &Hotkey::test_hotkey);
+	//ClassDB::bind_method(D_METHOD("initialize_hotkey_thread", "input"), &Hotkey::initialize_hotkey_thread);
 	BIND_CONSTANT(MOD_ALT);
 	BIND_CONSTANT(MOD_CONTROL);
 	BIND_CONSTANT(MOD_SHIFT);
 	BIND_CONSTANT(MOD_WIN);
-	
 }
 
-int Hotkey::test_hotkey() {
-	MSG msg = { 0 };
-	if (GetMessage(&msg, hWnd, 0, 0) != 0) {
-		if (msg.message == WM_HOTKEY) {
-			return msg.wParam;
-		}
-	}
-	return 0;
-}
+
 
 Hotkey::Hotkey() {
-	hWnd = GetActiveWindow();
 }
--- modules/hotkey/hotkey.h
+++ modules/hotkey/hotkey.h
@@ -3,6 +3,10 @@
 
 #include "core/reference.h"
 #include <Windows.h>
+#include "core/os/input_event.h"
+#include "main/input_default.h"
+#include "core/bind/core_bind.h"
+#include "core/variant.h"
 
 class Hotkey : public Reference {
 	GDCLASS(Hotkey, Reference);
@@ -10,12 +14,23 @@ class Hotkey : public Reference {
 protected:
 	static void _bind_methods();
 
+
 public:
-	void register_hotkey(int hotkey_id, int modifier, int virtual_key);
+	void register_hotkey(int hotkey_id, int modifier, int virtual_key, String input_name);
 	void unregister_hotkey(int hotkey_id);
-	int test_hotkey();
+	void test_hotkey();
+	//void initialize_hotkey_thread(InputDefault input);
 
 	HWND hWnd;
+	InputEventAction input_action;
+	_Thread thread;
+
+	struct input_map_entry {
+		String input_name;
+		int hotkey_id;
+	};
+
+	Vector<input_map_entry> input_map;
 
 	Hotkey();
 };
